947b8e7f6cd4179a2e48dc30a57a99db06e2aaed
yguenduez <yasinguenduez1@gmail.com>
2023-03-20 20:57:45 +0100

    drawing a canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,31 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys::console;
-
-
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
-
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
-
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+    context.move_to(300.0, 0.0); // top of triangle
+    context.begin_path();
+    context.line_to(0.0, 600.0); // bottom left of triangle
+    context.line_to(600.0, 600.0); // bottom right of triangle
+    context.line_to(300.0, 0.0); // back to top of triangle
+    context.close_path();
+    context.stroke();
+    context.fill();
     Ok(())
 }
