9124517b047e5d8ddabb17d1b50a0e0168932434
ysngndz <yasinguenduez1@gmail.com>
2023-03-22 19:46:07 +0100

    drawing first sprite with channel to await loading the first before drawing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+mod browser;
+
 use rand::prelude::*;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -83,11 +86,28 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
-    sierpinski(
-        &context,
-        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-        (0, 255, 0),
-        7,
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) =
+            futures::channel::oneshot::channel::<()>();
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move || {
+            success_tx.send(());
+        });
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        callback.forget();
+        image.set_src("Idle (1).png");
+
+        success_rx.await;
+
+        context.draw_image_with_html_image_element
+        (&image, 0.0, 0.0);
+        sierpinski(
+            &context,
+            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+            (0, 255, 0),
+            7,
+        );
+    }
     );
     Ok(())
 }
