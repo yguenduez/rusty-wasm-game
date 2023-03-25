479814de459620b726c8261121223383d35a6f24
ysngndz <yasinguenduez1@gmail.com>
2023-03-25 13:28:47 +0100

    update game methods and use them in the main fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 mod browser;
 mod engine;
+mod game;
 
 use rand::prelude::*;
 use std::collections::HashMap;
@@ -9,25 +10,9 @@ use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
+use crate::engine::GameLoop;
+use crate::game::WalkTheDog;
 use serde::Deserialize;
-
-#[derive(Deserialize)]
-struct Rect {
-    x: u16,
-    y: u16,
-    w: u16,
-    h: u16,
-}
-
-#[derive(Deserialize)]
-struct Cell {
-    frame: Rect,
-}
-
-#[derive(Deserialize)]
-struct Sheet {
-    frames: HashMap<String, Cell>,
-}
 
 async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
     let window = web_sys::window().unwrap();
@@ -41,48 +26,11 @@ async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    let context = browser::context().expect("Could not get browser context");
     browser::spawn_local(async move {
-        let sheet: Sheet = browser::fetch_json("rhb.json")
+        let game = WalkTheDog::new();
+        GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json")
-            .into_serde()
-            .expect(
-                "Could not convert rhb.json into a
-Sheet structure",
-            );
-
-        let image = engine::load_image("rhb.png")
-            .await
-            .expect("Could not load rhb.png");
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
-        }) as Box<dyn FnMut()>);
-
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            );
-        interval_callback.forget();
+            .expect("Could not start game loop")
     });
 
     Ok(())
