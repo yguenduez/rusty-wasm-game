821d1f0143a7908019ef87b348bad258c7856c86
yguenduez <yasinguenduez1@gmail.com>
2023-03-25 21:00:00 +0100

    Draw the rhb defined by the statemachine

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,11 @@
-use crate::engine::{Game, Renderer};
+use crate::engine::{Game, Rect, Renderer};
 use crate::{browser, engine};
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 use std::collections::HashMap;
 use web_sys::HtmlImageElement;
 
-use crate::game::red_hat_boy_states::{Idle, RedHatBoyState, Running};
+use crate::game::red_hat_boy_states::{Idle, RedHatBoyContext, RedHatBoyState, Running};
 use serde::Deserialize;
 
 #[derive(Deserialize, Clone)]
@@ -126,6 +126,7 @@ impl Game for WalkTheDog {
                 },
             )
         });
+        self.rhb.as_ref().unwrap().draw(renderer);
     }
 }
 
@@ -142,6 +143,35 @@ impl RedHatBoy {
             sprite_sheet: sheet,
             image,
         }
+    }
+
+    fn draw(&self, renderer: &Renderer) {
+        let frame_name = format!(
+            "{} ({}).png",
+            self.state_machine.frame_name(),
+            (self.state_machine.context().frame / 3) + 1
+        );
+
+        let sprite = self
+            .sprite_sheet
+            .frames
+            .get(&frame_name)
+            .expect("Cell not found");
+        renderer.draw_image(
+            &self.image,
+            &Rect {
+                x: sprite.frame.x.into(),
+                y: sprite.frame.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+            &Rect {
+                x: self.state_machine.context().position.x.into(),
+                y: self.state_machine.context().position.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+        );
     }
 }
 
@@ -162,6 +192,19 @@ impl RedHatBoyStateMachine {
             _ => self,
         }
     }
+
+    fn frame_name(&self) -> &str {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => state.frame_name(),
+            RedHatBoyStateMachine::Running(state) => state.frame_name(),
+        }
+    }
+    fn context(&self) -> &RedHatBoyContext {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => &state.context(),
+            RedHatBoyStateMachine::Running(state) => &state.context(),
+        }
+    }
 }
 
 impl From<RedHatBoyState<Running>> for RedHatBoyStateMachine {
@@ -174,11 +217,19 @@ mod red_hat_boy_states {
     use crate::game::Point;
 
     const FLOOR: i16 = 475;
+    const IDLE_FRAME_NAME: &str = "Idle";
+    const RUN_FRAME_NAME: &str = "Run";
 
     #[derive(Copy, Clone)]
     pub struct RedHatBoyState<S> {
         context: RedHatBoyContext,
         _state: S,
+    }
+
+    impl<S> RedHatBoyState<S> {
+        pub fn context(&self) -> &RedHatBoyContext {
+            &self.context
+        }
     }
 
     impl RedHatBoyState<Idle> {
@@ -200,13 +251,23 @@ mod red_hat_boy_states {
                 _state: Idle {},
             }
         }
+
+        pub fn frame_name(&self) -> &str {
+            IDLE_FRAME_NAME
+        }
+    }
+
+    impl RedHatBoyState<Running> {
+        pub fn frame_name(&self) -> &str {
+            RUN_FRAME_NAME
+        }
     }
 
     #[derive(Copy, Clone)]
     pub struct RedHatBoyContext {
-        frame: u8,
-        position: Point,
-        velocity: Point,
+        pub frame: u8,
+        pub position: Point,
+        pub velocity: Point,
     }
 
     #[derive(Copy, Clone)]
