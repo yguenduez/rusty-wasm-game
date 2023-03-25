86b7676f673c92750096efffe3640512d0b0f253
ysngndz <yasinguenduez1@gmail.com>
2023-03-25 12:54:04 +0100

    add a renderer to the draw method

--- src/engine.rs
+++ src/engine.rs
@@ -41,7 +41,7 @@ pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
 pub trait Game {
     async fn initialize(&self) -> Result<Box<dyn Game>>;
     fn update(&mut self);
-    fn draw(&self, context: &CanvasRenderingContext2d);
+    fn draw(&self, renderer: &Renderer);
 }
 
 type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
@@ -52,6 +52,11 @@ impl GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
         };
+
+        let renderer = Renderer {
+            context: browser::context()?,
+        };
+
         let f: SharedLoopClosure = Rc::new(RefCell::new(None));
         let g = f.clone();
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
@@ -61,7 +66,7 @@ impl GameLoop {
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
-            game.draw(&browser::context().expect("Context should exist"));
+            game.draw(&renderer);
             browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
         browser::request_animation_frame(
@@ -77,4 +82,42 @@ const FRAME_SIZE: f32 = 1.0 / 60.0 * 1000.0;
 pub struct GameLoop {
     last_frame: f64,
     accumulated_delta: f32,
+}
+
+pub struct Renderer {
+    context: CanvasRenderingContext2d,
+}
+
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Renderer {
+    pub fn clear(&self, rect: &Rect) {
+        self.context.clear_rect(
+            rect.x.into(),
+            rect.y.into(),
+            rect.width.into(),
+            rect.height.into(),
+        );
+    }
+
+    pub fn draw_image(&self, image: &HtmlImageElement, frame: &Rect, destination: &Rect) {
+        self.context
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                frame.x.into(),
+                frame.y.into(),
+                frame.width.into(),
+                frame.height.into(),
+                destination.x.into(),
+                destination.y.into(),
+                destination.width.into(),
+                destination.height.into(),
+            )
+            .expect("Drawing is throwing exceptions! Unrecoverable error.");
+    }
 }
